b774dc2cef8c5f05064d34d9e7f68a2e650d6ee2
Rashed <rashedalamj@gmail.com>
2024-02-07 23:16:19 +0600

    remove google fonts & add system font

--- Resources/View/frontend/common/header.php
+++ Resources/View/frontend/common/header.php
@@ -8,9 +8,6 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:ital,wght@0,400;0,600;1,500&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="<?php echo assets("css/custom.css"); ?>" />
     <link rel="shortcut icon" href="<?php echo stored_file("favicon/" . get_favicon()); ?>" type="image/x-icon" />
 </head>
